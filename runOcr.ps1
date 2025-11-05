081d76141694439c549ecbe659d33d727a266d6d
pyro5ive <steve@nolabizit.com>
2025-11-04 19:02:53 -0600

    remove

--- runOcr.ps1
+++
@@ -1,7 +0,0 @@
-$source = "C:\Users\steve\"OneDrive - NOLA Business IT"\source\repos\grocery-ml\dec
-$ocrParams = (Get-ChildItem -Path $source -Filter *.png -File | Sort-Object Name | ForEach-Object { "-i `"$($_.FullName)`"" }) -join " "
-
-
-
-
-& C:\Users\steve\Downloads\capture2text_4.6.3\Capture2Text_CLI.exe --line-breaks $ocrParams
