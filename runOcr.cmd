aa564229fe278b4e1047b001e4613f61fcd51799
pyro5ive <steve@nolabizit.com>
2025-11-04 18:56:17 -0600

    Initial commit

---
+++ runOcr.cmd
@@ -0,0 +1,15 @@
+
+
+setlocal enabledelayedexpansion
+set "source=C:\Users\steve\OneDrive - NOLA Business IT\source\repos\grocery-ml\dec"
+set "params="
+set "format= --output-format "${capture}${linebreak}${linebreak}${linebreak}####${linebreak}${linebreak}${linebreak} ""
+set "outfile= -o ocrtext.txt --output-file-append"
+set "tesscfg=  --tess-config-file tess.cfg "
+for %%F in ("%source%\*.png") do set "params=!params! -i "%%~fF""
+
+del ocrtext.txt
+C:\Users\steve\Downloads\capture2text_4.6.3\Capture2Text_CLI.exe  !tesscfg! !format! !outfile!  --line-breaks  !params!
+endlocal
+
+
